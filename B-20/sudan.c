444274eee0995389db5e4bdcfea21edc8403a5ad
Pepich <benedikt.abel@yahoo.de>
2016-07-07 09:45:05 +0200

    a

--- B-20/sudan.c
+++ B-20/sudan.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 
+// Recursive implementation of the sudan function
 int sudan(int x, int y, int z)
 {
     if (z == 0)
@@ -22,16 +23,21 @@ int sudan(int x, int y, int z)
 int main(void)
 {
     int x = 0, y = 0;
+    // Printing output grid header
     printf("x\\y|   0   1   2   3   4   5\n");
     printf("----------------------------");
     for (x = 0; x <= 5; x++)
     {
+	// Printing output grid first column entry
 	printf("\n %i |", x);
 	for (y = 0; y <= 5; y++)
 	{
+	    // Output the result of sudan(x,y,1) formatted to match the grid spacing
 	    printf(" %3i",sudan(x, y, 1));
 	}
     }
+    // Clean finish \n output
     printf("\n");
+    // End
     return 0;
 }
--- B-22/arrays.c
+++ B-22/arrays.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 
+// Subroutine to read values with a single line
 double readVal(int n)
 {
     double d = 0;
@@ -18,39 +19,56 @@ double readVal(int n)
     return d;
 }
 
-double minmax(double values[], int size, double *min, double *max)
+// Funktion to calculate the average as return value, as well as min/max as referenced parameters
+double avg(double values[], int size, double *min, double *max)
 {
+    // Initializing counter variable
     int i = 0;
-    double val = values[0], avg = val;
+    // Initializing variables to have a starting value for proper comparison
+    double val = values[0], total = val;
     *min = val;
     *max = val;
     for (i = 1; i < size; i++)
     {
+	// Grabbing the next value into a temp variable for faster access
 	val = values[i];
+	
+	// Finding the smalles value
 	if (val < *min)
 	    *min = val;
+	// Finding the biggest value
 	if (val > *max)
 	    *max = val;
-	avg += val;
+	// Adding the current value to the total value
+	total += val;
     }
-    return avg/size;
+    // Average is total/amount
+    return total/size;
 }
 
 int main(void)
 {
+    // Initializing variables
     int n = 0, i = 0;
     double min = 0, max = 0, avg = 0;
+    // Requesting length of array input
     printf("Bitte länge des Feldes eingeben: ");
     scanf("%i", &n);
+    //Creating proper sized array
     double values[n];
+    
+    // Fill the array with values using the "readVal()" subroutine
     for (i = 0; i < n; i++)
-    {
 	values[i] = readVal(i);
-    }
-    avg = minmax(values, n, &min, &max);
+    
+    // Compute the average, as well as min and max of the array using the avg() function
+    avg = avg(values, n, &min, &max);
+
+    //  Final output
     printf("Die Anzahl der Einträge ist: %i\n"
 	   "Der größte Eintrag hat den Wert: %g\n"
 	   "Der kleinste Eintrag hat den Wert: %g\n"
 	   "Der Durchschnitt aller Werte beträgt: %g\n", n, max, min, avg);
+    // End
     return 0;
 }
